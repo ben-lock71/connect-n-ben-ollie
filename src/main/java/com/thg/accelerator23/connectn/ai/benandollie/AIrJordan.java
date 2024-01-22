f7e078f65bae99ad4fa914066d0e1540aef29c6d
Ben Lock <ben.lock71@thehutgroup.com>
2024-01-22 11:09:14 +0000

    New player

--- src/main/java/com/thg/accelerator23/connectn/ai/benandollie/AIrJordan.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/benandollie/AIrJordan.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.benandollie;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class AIrJordan extends Player {
+  public AIrJordan(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, AIrJordan.class.getName());
   }
 
   @Override
