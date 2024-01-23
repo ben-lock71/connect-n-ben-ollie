7061147a5f1d936d3c25e7ba99eafa539d79d726
Ben Lock <ben.lock71@thehutgroup.com>
2024-01-23 14:30:25 +0000

    Amended protected method reflection

--- src/main/java/com/thg/accelerator23/connectn/ai/benandollie/AIrJordan.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/benandollie/AIrJordan.java
@@ -5,9 +5,12 @@ import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 import com.thehutgroup.accelerator.connectn.player.Position;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+
 
 public class AIrJordan extends Player {
   public AIrJordan(Counter counter) {
@@ -19,8 +22,9 @@ public class AIrJordan extends Player {
     StringBuilder boardState = new StringBuilder();
     StringBuilder XState = new StringBuilder();
     try {
-      board.getClass().getDeclaredField("counterPlacements").setAccessible(true);
-      Counter[][] counters = board.getCounterPlacements();
+      Method getCounterBoard = board.getClass().getDeclaredMethod("getCounterPlacements", Board.class);
+      getCounterBoard.setAccessible(true);
+      Counter[][] counters = (Counter[][]) getCounterBoard.invoke(null, board);
       List<List<Counter>> columns = Arrays.stream(counters)
               .map(Arrays::asList).toList();
       for (List<Counter> row : columns) {
@@ -38,12 +42,16 @@ public class AIrJordan extends Player {
         boardState.append(emptyString);
 
       }
-    } catch (NoSuchFieldException e) {
+    } catch (NoSuchMethodException e) {
       throw new RuntimeException(e);
+    } catch (InvocationTargetException e) {
+        throw new RuntimeException(e);
+    } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
     }
-    String s1 = String.valueOf(boardState);
+      String s1 = String.valueOf(boardState);
     String s2 = String.valueOf(XState);
-    StringBuffer OMoves = new StringBuffer();
+    StringBuilder OMoves = new StringBuilder();
     for (int i = 0; i < s1.length(); i++) {
       OMoves.append(s1.charAt(i)^s2.charAt(i));
     }
